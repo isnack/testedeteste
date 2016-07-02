93517cfeb3de32ce9d899c84f6f0cc78de515b1f
isnack <isnack_yagami@hotmail.com>
2016-07-02 00:03:32 -0300

    Update test2.js

--- Test/test2.js
+++ Test/test2.js
@@ -12,7 +12,7 @@ casper.test.begin('Google search retrieves 10 or more results', 5, function suit
         test.assertTitle("casperjs - Recherche Google", "google title is ok");
         test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
         test.assertEval(function() {
-            return __utils__.findAll("h3.r").length <= 10;
+            return __utils__.findAll("h3.r").length >= 10;
         }, "google search for \"casperjs\" retrieves 10 or more results");
     });
 
